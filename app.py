7952113d1ce12cb09bbb798b6b3977a4c24fa50d
b10715041 <b10715041@gmail.com>
2024-05-29 19:30:17 +0900

    Add close and settings btn. Showing datetime.

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
 import tkinter as tk
+import tkinter.font as tkFont
 from datetime import datetime
 import threading
 import pystray
 from PIL import Image, ImageDraw, ImageTk
 
+class CanvasButton:
+    def __init__(self, canvas, x, y, anchor, image, image_hover, command):
+        self.canvas = canvas
+        self.image = tk.PhotoImage(file=image)
+        self.image_hover = tk.PhotoImage(file=image_hover)
+        self.command = command
+        self.img = canvas.create_image(x, y, anchor=anchor, image=self.image)
+        self.canvas.tag_bind(self.img, '<Enter>', self.on_enter)
+        self.canvas.tag_bind(self.img, '<Leave>', self.on_leave)
+        self.canvas.tag_bind(self.img, '<ButtonRelease-1>', self.on_click)
+
+    def on_click(self, event):
+        self.command()
+
+    def on_enter(self, event):
+        self.canvas.itemconfig(self.img, image=self.image_hover)
+
+    def on_leave(self, event):
+        self.canvas.itemconfig(self.img, image=self.image)
+
 class FloatingApp:
     def __init__(self, root):
         self.root = root
         self.root.overrideredirect(True)  # Remove window border
-        self.root.attributes('-topmost', True)  # Keep the window on top
+        self.root.attributes('-topmost', True)
 
-        self.bg_image = Image.open("img/f2.png")  # Load the background image
-        self.bg_photo = ImageTk.PhotoImage(self.bg_image)  # Convert to PhotoImage for Tkinter
-
-        # Adjust the window size to the image size
-        self.root.geometry(f'{self.bg_image.width}x{self.bg_image.height}')
+        self.bg_image = tk.PhotoImage(file="img/bg.png")
+        self.avatar_image = tk.PhotoImage(file="img/erika.png")
 
         self.create_widgets()
         self.update_time()
@@ -31,37 +49,49 @@ class FloatingApp:
         self.root.wm_attributes('-transparentcolor', self.root['bg']) 
 
     def create_widgets(self):
-        # Create a label with the background image
-        self.bg_label = tk.Label(self.root, image=self.bg_photo, bg=self.root['bg'])  # Set 'white' to match the transparent color
-        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.canvas = tk.Canvas(self.root, width=320, height=195)
+        self.canvas.pack(fill='both', expand=True)
+        
+        self.canvas.create_image(0, 195, image=self.bg_image, anchor='sw')
+        self.avatar = self.canvas.create_image(320, 200, image=self.avatar_image, anchor='se')
 
-        # Create a label to display the current time
-        self.label = tk.Label(self.root, text='', font=('Helvetica', 12), bg='white', fg='black')
-        self.label.pack()
+        # current time
+        self.time = self.canvas.create_text(20, 115, text='', font=('なつめもじ', 20), fill='black', anchor='w', tags='time')
 
-        # Create a button to hide the application
-        self.hide_button = tk.Button(self.root, text='Hide', command=self.hide_app, bg='white', fg='black')
-        self.hide_button.pack()
+        self.hide_button = CanvasButton(self.canvas, 20, 190, 'sw', 'img/close.png', 'img/close_hover.png', self.hide_app)
+        self.settings_button = CanvasButton(self.canvas, 40, 190, 'sw', 'img/settings.png', 'img/settings_hover.png', 0)
+
 
     def update_time(self):
         # Update the label with the current date and time
-        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        self.label.config(text=now)
+        current_time = datetime.now().strftime('  %H:%M:%S')
+
+        days_in_jp = ['月', '火', '水', '木', '金', '土', '日']
+        day_of_week = days_in_jp[datetime.now().weekday()]
+        current_date = datetime.now().strftime(f'%m月%d日\n      ({day_of_week})')
+
+        self.canvas.itemconfigure(self.time, text=f"{current_time}\n{current_date}")
         self.root.after(1000, self.update_time)  # Update every second
 
     def start_move(self, event):
-        # Remember the initial position of the window
-        self.x = event.x
-        self.y = event.y
+        if event.widget == self.canvas:
+            item = self.canvas.find_withtag('current')
+            if 'canvas_btn' in self.canvas.gettags(item):
+                return
+            # Calculate absolute position of the cursor on screen
+            self.x = self.root.winfo_pointerx() - self.root.winfo_rootx()
+            self.y = self.root.winfo_pointery() - self.root.winfo_rooty()
 
     def do_move(self, event):
-        # Move the window to the new position
-        x = self.root.winfo_pointerx() - self.x
-        y = self.root.winfo_pointery() - self.y
-        self.root.geometry(f'+{x}+{y}')
+        if event.widget == self.canvas:
+            item = self.canvas.find_withtag('current')
+            if 'canvas_btn' in self.canvas.gettags(item):
+                return
+            x = self.root.winfo_pointerx() - self.x
+            y = self.root.winfo_pointery() - self.y
+            self.root.geometry(f'+{x}+{y}')
 
-    def hide_app(self):
-        # Hide the application window
+    def hide_app(self, event=None):
         self.root.withdraw()
 
     def exit_app(self, icon=None, item=None):
@@ -71,10 +101,7 @@ class FloatingApp:
         self.root.after(0, self.root.quit)
 
     def create_tray_icon(self):
-        # Load the icon for the tray
         icon_image = Image.open("img/icon.ico").convert('RGBA')
-
-        # Convert the icon to the appropriate format for pystray
         return pystray.Icon(
             name='WaifuClock',
             icon=icon_image,
